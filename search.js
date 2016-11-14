629b08230028ca047e32d19da1512ab2015579c9
Avi <aviralg@interchange.ubc.ca>
2016-11-13 20:53:07 -0800

    Update search.js

--- search.js
+++ search.js
@@ -1,10 +1,12 @@
 // After the API loads, call a function to enable the search box.
 function handleAPILoaded() {
+  console.log("handleAPIloaded");
   $('#search-button').attr('disabled', false);
 }
 
 // Search for a specified string.
 function search() {
+  console.log("search");
   var q = $('#query').val();
   var request = gapi.client.youtube.search.list({
     q: q,
@@ -12,14 +14,17 @@ function search() {
   });
 
   request.execute(function(response) {
+    console.log("execute");
     var str = JSON.stringify(response.result);
     $('#search-container').html('<pre>' + str + '</pre>');
   });
 }
 
 function init() {
+  console.log("init");
     gapi.client.setApiKey("AIzaSyAV1MZol0Lh9yA1i9EKAWcuYLI3hjqLj3Y");
     gapi.client.load("youtube", "v3", function() {
         // yt api is ready
+      console.log("load");
     });
 }
